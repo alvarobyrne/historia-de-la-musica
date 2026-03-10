00700a7d2c30e1ab75af97ba4f3c8641a2726528
Alvaro O'Byrne <alvarobyrne@gmail.com>
2026-03-10 18:42:05 -0500

    Add build configuration for Rollup input options

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
     base: 'https://alvarobyrne.github.io/historia-de-la-musica/',
+    build:{
+        rollupOptions:{
+            input: {
+                main: 'index.html',
+                barroco: 'barroco.html',
+                clasicismo: 'clasicismo.html',
+                romanticismo: 'romanticismo.html'
+            }   
+        }
+    }
 });
